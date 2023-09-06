a821d5333fd5cd121e57c1c31e2ae8b5113313fe
DeutschRitter <deutschritter78@me.com>
2023-09-06 10:20:42 +0200

    doc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,10 @@
-<form name="contact" action="/danke" method="POST" data-netlify="true">
-	<input type="hidden" name="form-name" value="contact" />
-
-	<div>
-		<label for="name">Name:</label>
-		<input type="text" id="name" name="name" required />
-	</div>
-
-	<div>
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email" required />
-	</div>
-
-	<div>
-		<label for="message">Nachricht:</label>
-		<textarea id="message" name="message" required />
-	</div>
-
-	<div>
-		<button type="submit">Senden</button>
-	</div>
+<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+	<input type="hidden" name="form-name" value="netlify-form-example" />
+	<label for="name">Name</label>
+	<input name="name" id="name" required placeholder="Name" type="text" />
+	<label for="email">Email</label>
+	<input name="email" id="email" required placeholder="Email" type="email" />
+	<label for="message">Message</label>
+	<input name="message" id="message" required placeholder="Message" type="text" />
+	<input type="submit" value="Submit" />
 </form>
