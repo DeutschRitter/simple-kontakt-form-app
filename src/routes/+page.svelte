4f88460784a9e30cb192c2efc88d477493b30cc5
DeutschRitter <deutschritter78@me.com>
2023-09-06 09:57:48 +0200

    form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,11 @@
-<form name="contact" netlify netlify-honeypot="bot-field" on:submit={handleSubmit}>
-	<p hidden>
-		<label>
-			Don’t fill this out if you’re human: <input name="bot-field" />
-		</label>
+<form name="contact" netlify>
+	<p>
+		<label>Name <input type="text" name="name" /></label>
 	</p>
-
-	<label for="name">Name1:</label>
-	<input type="text" id="name" name="name" required bind:value={name} />
-
-	<label for="email">E-Mail-Adresse:</label>
-	<input type="email" id="email" name="email" required bind:value={email} />
-
-	<label for="subject">Betreff:</label>
-	<input type="text" id="subject" name="subject" required bind:value={subject} />
-
-	<label for="message">Nachricht:</label>
-	<textarea id="message" name="message" required bind:value={message} />
-
-	<button type="submit">Senden</button>
+	<p>
+		<label>Email <input type="email" name="email" /></label>
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
 </form>
