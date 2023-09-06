7215272dffbda54a3822796ec7fa4bde7d1c993b
DeutschRitter <deutschritter78@me.com>
2023-09-06 11:09:02 +0200

    popup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
+<script>
+	let showPopup = false;
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+		const response = await fetch('/danke', {
+			method: 'POST',
+			body: formData
+		});
+
+		if (response.ok) {
+			showPopup = true;
+		} else {
+			// Fehlerbehandlung, z.B. eine Fehlermeldung anzeigen
+			console.error('Form submission failed');
+		}
+	}
+</script>
+
 <form
-	action="/danke"
+	on:submit={handleSubmit}
 	name="netlify-form-example"
-	method="POST"
 	netlify-honeypot="bot-field"
 	data-netlify="true"
 >
@@ -14,3 +34,23 @@
 	<input name="message" id="message" required placeholder="Message" type="text" />
 	<input type="submit" value="Submit" />
 </form>
+
+{#if showPopup}
+	<div class="popup">
+		Danke! Ihre Nachricht wurde versendet.
+		<button on:click={() => (showPopup = false)}>Schließen</button>
+	</div>
+{/if}
+
+<style>
+	.popup {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		padding: 20px;
+		background-color: #fff;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		z-index: 1000;
+	}
+</style>
