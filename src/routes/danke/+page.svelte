a90b8e5593379efbb9e43135d45216b3c5feb1a8
DeutschRitter <deutschritter78@me.com>
2023-09-06 10:10:13 +0200

    danke

---
+++ src/routes/danke/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// Optional: Hier kannst du eine Logik einfügen, die z.B. nach einigen Sekunden zurück zur Startseite weiterleitet
+	});
+</script>
+
+<h1>Danke für deine Nachricht!</h1>
+<p>Wir werden uns so schnell wie möglich bei dir melden.</p>
