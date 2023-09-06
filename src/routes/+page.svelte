906f0bf7a56e286419fa758cd783be8c14c39ed4
DeutschRitter <deutschritter78@me.com>
2023-09-06 10:05:48 +0200

    forms 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
-<form name="contact" netlify>
-	<p>
-		<label>Name <input type="text" name="name" /></label>
-	</p>
-	<p>
-		<label>Email <input type="email" name="email" /></label>
-	</p>
-	<p>
-		<button type="submit">Send</button>
-	</p>
+<form name="contact" action="/danke" method="POST" data-netlify="true">
+	<input type="hidden" name="form-name" value="contact" />
+
+	<div>
+		<label for="name">Name:</label>
+		<input type="text" id="name" name="name" required />
+	</div>
+
+	<div>
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" required />
+	</div>
+
+	<div>
+		<label for="message">Nachricht:</label>
+		<textarea id="message" name="message" required />
+	</div>
+
+	<div>
+		<button type="submit">Senden</button>
+	</div>
 </form>
